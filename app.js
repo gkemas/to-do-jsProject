61e4f93faf8f2a31289ae2c5b965da30a45ceb2a
gokhan <kemasgokhan@gmail.com>
2022-06-07 23:03:08 +0300

    ad

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-const giris =document.getElementById("giris");
-const ekle =document.getElementById("ekle");
-const list =document.getElementById("liste");
+const giris = document.getElementById("giris");
+const ekle = document.getElementById("ekle");
+const list = document.getElementById("liste");
+// const check = document.getElementsByClassName("check");
+ekle.addEventListener("click", function () {
+  if (!giris.value) {
+    alert("please write something");
+  } else {
+    add();
+    giris.innerHTML = giris.value = "";
+    let close = document.querySelectorAll(".delete");
 
-
-ekle.addEventListener("click",function(){
-   console.log("e");
-   add();
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        this.parentElement.remove();
+      };
+    }
+    let check = document.querySelectorAll(".check");
+    console.log(check);
+    for (let i = 0; i < check.length; i++) {
+      check[i].onclick = function () {
+        console.log("onclick");
+        this.parentElement.style.textDecoration = "line-through";
+        // this.parentElement.firstChild.innerText.style.opacity = "0.5";
+        this.nextElementSibling.style.color = "green";
+        this.style.backgroundColor = "green";
+      };
+    }
+    for (let i = 0; i < close.length; i++) {
+      check[i].ondblclick = function () {
+        this.parentElement.style.textDecoration = "none";
+        this.nextElementSibling.style.color = "black";
+      };
+    }
+  }
 });
-function add(){
 
-    row =`  <input type="checkbox" name="todo" id="">${giris.value}</input>   `
- }
+function add() {
+  row = ` <di class="check1" > <input type="checkbox" name="todo" id="" class="check">${giris.value}</input> <button class="delete">
+  <i class="far fa-trash-alt"></i>
+  </button> </div> <br> `;
+
+  list.innerHTML += row;
+}
+
+window.onload = () => {
+  giris.focus;
+};
+
+giris.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    ekle.click();
+  }
+});
+
+// list.addEventListener("click", (e) => {
+//    e.target.nextSibling.style.textDecoration = "line-through";
+//    console.log("dd");
+//  });
